acc9709b1936422bd1a2be8c630edff9f5a1839e
Gabriel Sato <gabriel_sato@hotmail.com>
2013-09-05 17:43:05 -0300

    Servidor alterações

--- servidor.py
+++ servidor.py
@@ -2,6 +2,52 @@
 #coding=utf-8
 import socket
 import sys
+import os
+
+class Tabuleiro:
+    posicao = 0
+    tabuleiro=[0]*8
+
+    def Movimenta(self,x):
+        #print "movimentou: %d" % x
+        # cima
+        if x == 1:  
+            self.posicao = ((self.posicao - 3) % 9)
+        
+        # baixo
+        elif x == 2:
+            self.posicao = ((self.posicao + 3) % 9)
+        
+        # direita
+        elif x == 3:
+            if ((self.posicao + 1) % 3) == 0:
+                self.posicao -= 2 
+            else:
+                self.posicao += 1
+        #esquerda
+        elif x == 4:
+            if ((self.posicao % 3) == 0):
+                self.posicao += 2 
+            else:
+                self.posicao -= 1
+        
+        else:
+            print "Movimentação errada"
+
+        return self.posicao
+
+    def Zera(self,posicao):
+        self.tabuleiro = [0]*8
+        return (self.tabuleiro).insert(posicao,1)
+
+    def Imprime(self):
+        for x in xrange(0,3):
+            for y in xrange(0,3):
+                if self.tabuleiro[(x*3)+y] == 1:
+                    sys.stdout.write("_X|")
+                else:
+                    sys.stdout.write("__|")
+            print
 
 def main():
     if len(sys.argv) < 2:
@@ -14,9 +60,16 @@ def main():
     orig=(HOST, PORT)
     udp.bind(orig)
     try:
+        campo = Tabuleiro()
         while(1):
             msg, cliente = udp.recvfrom(1024)
-            print cliente, ': ', msg
+            os.system('clear')
+            campo.Movimenta(int(msg))
+            campo.Zera(campo.posicao)
+            campo.Imprime()
+            msg = ' '.join(str(e) for e in campo.tabuleiro)
+            udp.sendto(msg, cliente)
+
     except KeyboardInterrupt:
         udp.close()
         sys.exit()
