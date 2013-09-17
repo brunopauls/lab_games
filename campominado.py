5740d7c9c2f7b80ed282e860f84faf10455053d9
Gabriel Sato <gabriel_sato@hotmail.com>
2013-09-17 09:04:57 -0300

    Arrumados bugs

--- campominado.py
+++ campominado.py
@@ -3,14 +3,21 @@
 
 import random
 import sys
+class Jogador():
+	bombas=0
+	endereco=(0, 0)
+
 
 class Tabuleiro:
 	bombas=[0]*21
 	ganhador=0
+	jogador_1=Jogador()
+	jogador_2=Jogador()
 
 	def __init__(self):
 		self.tabuleiro__ = []
 		self.tabuleiro = []
+		self.jogador_vez=self.jogador_1
 
 		for i in range(0,15):
 			vetor = [0]*15
@@ -30,6 +37,20 @@ class Tabuleiro:
 			for j in range(0, 15):
 				if self.tabuleiro__[i][j]==9:
 					self.TrataBomba(i, j)
+
+	def Jogada(self, x, y):
+		self.tabuleiro[x][y]=self.tabuleiro__[x][y]
+		if self.tabuleiro__[x][y]==9:
+			self.jogador_vez.bombas+=1
+			return False
+		return True
+
+	def TrocaJogadores(self):
+		if self.jogador_vez == self.jogador_1:
+			self.jogador_vez = self.jogador_2
+		else:
+			self.jogador_vez = self.jogador_1
+
 
 	def TrataBomba(self, linha, coluna):
 		try:
@@ -109,5 +130,17 @@ def main():
 	campo.ArrumaCampo()
 	campo.ImprimeCampoRevelado()
 	campo.ImprimeCampoNaoRevelado()
+	while(1):
+		print vars(campo.jogador_vez)
+		print 'Posicao 1'
+		x = raw_input()
+		print 'Posicao 2'
+		y = raw_input()
+		troca = campo.Jogada(int(x), int(y))
+		if troca:
+			campo.TrocaJogadores()
+		campo.ImprimeCampoRevelado()
+		campo.ImprimeCampoNaoRevelado()
+
 
 if __name__ == '__main__': main()
