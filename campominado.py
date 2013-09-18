508952cb05da72dd85e799d4e52d2097d08c85c6
Gabriel Sato <gabriel_sato@hotmail.com>
2013-09-17 22:36:02 -0300

    Programa basico funcionando, falta a movimentacao do jogo, e o envia de mensagem!

--- campominado.py
+++ campominado.py
@@ -1,53 +1,72 @@
 #!/usr/bin/python
 #coding=utf-8
-
 import random
 import sys
+
 class Jogador():
 	bombas=0
 	endereco=(0, 0)
 
+	def __init__(self, nome):
+		self.nome = nome
 
 class Tabuleiro:
-	bombas=[0]*21
+	tamanho=15
+	bombas=21
 	ganhador=0
-	jogador_1=Jogador()
-	jogador_2=Jogador()
+	jogador_1=Jogador('Jogador 1')
+	jogador_2=Jogador('Jogador 2')
 
 	def __init__(self):
 		self.tabuleiro__ = []
 		self.tabuleiro = []
 		self.jogador_vez=self.jogador_1
 
-		for i in range(0,15):
-			vetor = [0]*15
-			vetor2 = [-1]*15
+		for i in range(0,self.tamanho):
+			vetor = [0]*self.tamanho
+			vetor2 = [-1]*self.tamanho
 			self.tabuleiro__.append(vetor)
 			self.tabuleiro.append(vetor2)
 
-	def CriaBombas(self):
-		self.bombas = sorted([x for x in random.sample(range(0, 225), 21)])
+	def TransformaMatriz(self, lista):
+		nova_lista=[]
+		lista_atual=[]
+		for i in range(0, len(lista)):
+			if (i%self.tamanho == 0) and (i>0):
+				nova_lista.append(lista_atual)
+				lista_atual=[]
+			lista_atual.append(lista.pop(0))
+		nova_lista.append(lista_atual)
+		return nova_lista
+
+	def InverteMatrizLista(self, lista):
+		if isinstance(lista, list):
+			if isinstance(lista[0], list):
+				return sum(self.tabuleiro__, [])
+			else:
+				return self.TransformaMatriz(lista)
+		return None
 
 	def ColocaBombas(self):
-		for bomba in self.bombas:
-			self.tabuleiro__[(bomba/15)][(bomba%15)] = 9
+		for bomba in sorted([x for x in random.sample(range(0, (self.tamanho*self.tamanho)), self.bombas)]):
+			self.tabuleiro__[(bomba/self.tamanho)][(bomba%self.tamanho)] = 9
+
+	def TrataBomba(self, linha, coluna):
+		if not ((linha < 0 or linha >= self.tamanho) or (coluna < 0 or coluna >= self.tamanho)):
+			if self.tabuleiro__[linha][coluna] <> 9:
+				self.tabuleiro__[linha][coluna]+=1
 
 	def ArrumaCampo(self):
-		for i in range(0, 15):
-			for j in range(0, 15):
+		for i in range(0, self.tamanho):
+			for j in range(0, self.tamanho):
 				if self.tabuleiro__[i][j]==9:
-					print i, j
-					k=-1
-					while (k <= 1):
-						l=-1
-						while (l <= 1):
+					for k in range(-1, 2):
+						for l in range(-1, 2):
 							self.TrataBomba(i+k, j+l)
-							l+=1
-						k+=1
 
 	def ChecaVazios(self, x, y):
 		try:
-			if (x < 0 or x > 14) or (y < 0 or y > 14):
+			if (x < 0 or x >= self.tamanho) or (y < 0 or y >= self.tamanho):
 				return
 			if (self.tabuleiro__[x][y]==0) and (self.tabuleiro[x][y]<>0):
 				self.tabuleiro[x][y]=0
@@ -66,11 +85,12 @@ class Tabuleiro:
 		except:
 			return
 
-
 	def AbreVazios(self, x, y):
 		self.ChecaVazios(x, y)
 
 	def Jogada(self, x, y):
+		if self.tabuleiro[x][y]<>-1:
+			return False
 		if self.tabuleiro__[x][y]==0:
 			self.AbreVazios(x, y)
 		elif self.tabuleiro__[x][y]==9:
@@ -86,16 +106,6 @@ class Tabuleiro:
 		else:
 			self.jogador_vez = self.jogador_1
 
-
-	def TrataBomba(self, linha, coluna):
-		try:
-			if not ((linha < 0 or linha > 14) or (coluna < 0 or coluna > 14)):
-				if self.tabuleiro__[linha][coluna] <> 9:
-					print linha, coluna
-					self.tabuleiro__[linha][coluna]+=1
-		except:
-			pass
-
 	def ImprimeCampoRevelado(self):
 		print
 		for vetor in self.tabuleiro__:
@@ -103,34 +113,34 @@ class Tabuleiro:
 		print
 
 	def ImprimeCampoNaoRevelado(self):
-		print
+		print '-----------------------------------------------'
 		for vetor in self.tabuleiro:
+			sys.stdout.write('|')
 			for elemento in vetor:
 				string = str(elemento) if elemento<>-1 else ' '
 				sys.stdout.write(" %s " % string)
-			print
-		print
-
+			print '|'
+		print '-----------------------------------------------'
 
 def main():
 	campo = Tabuleiro()
-	campo.CriaBombas()
 	campo.ColocaBombas()
 	campo.ArrumaCampo()
 	campo.ImprimeCampoRevelado()
 	campo.ImprimeCampoNaoRevelado()
 
+	"""
 	while(1):
-		print vars(campo.jogador_vez)
-		print 'Posicao 1'
+		print '%s (Bombas: %s)' % (campo.jogador_vez.nome, str(campo.jogador_vez.bombas))
+		sys.stdout.write('Linha [0..14]: ')
 		x = raw_input()
-		print 'Posicao 2'
+		sys.stdout.write('Coluna [0..14]: ')
 		y = raw_input()
 		troca = campo.Jogada(int(x), int(y))
 		if troca:
 			campo.TrocaJogadores()
 		campo.ImprimeCampoRevelado()
 		campo.ImprimeCampoNaoRevelado()
-	
+	"""
 
 if __name__ == '__main__': main()
